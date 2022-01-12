ecb62c9e23372eb1c6d5414d32112c88ded9a893
ikmal <ikmalazman65@gmail.com>
2022-01-12 12:01:00 +0000

    Client

--- Client.py
+++ Client.py
@@ -4,10 +4,53 @@ PORT = 8889
 
 print("Welcome to Speed ROCK, PAPER, SCISSOR\n")
 
+def rocks(x,y):
+
+      move = x
+      opponent = y
+
+      if move == opponent:
+              print( "It's Tie\n")
+      elif move == 'rocks' and opponent == 'scissors':
+              print("You win\n")
+      elif move == 'rocks' and opponent == 'paper':
+              print("You lose\n")
+      else:
+              print("Invalid move Rocks\n")
+
+def paper(x,y):
+
+      move = x
+      opponent = y
+
+      if move == opponent:
+               print("It's a Tie\n")
+      elif move == 'paper' and opponent == 'scissors':
+               print("You lose\n")
+      elif move == 'paper' and opponent == 'rocks':
+               print("You win\n")
+      else:
+           print("Invalid move Paper\n")
+
+def scissor(x,y):
+
+      move = x
+      opponent = y
+
+      if move == opponent:
+               print("It's a Tie\n")
+      elif move == 'scissors' and opponent == 'paper':
+               print("You win\n")
+      elif move == 'scissors' and opponent == 'rocks':
+               print("You lose\n")
+      else:
+               print("Invalid move Scissors")
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         while True:
-               move = input('Rocks, Paper, Scissors or Exit : ')
+               move = input('rocks, paper,scissors or exit : ')
                s.sendall(str.encode(move))
                
                data = b""
@@ -21,37 +64,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                
                print("Received:", opponent)
 
-               if move == "Rocks":
-                       if move == opponent:
-                             print( "It's Tie\n")
-                       elif move == 'Rocks' and opponent == 'Scissors':
-                             print("You win\n")
-                       elif move == 'Rocks' and opponent == 'Paper':
-                             print("You lose\n")
-                       else:
-                             print("Invalid move Rocks\n")
+               if move == "rocks":
+                       rocks(move,opponent)
 
-               elif move == 'Paper':
-                       if move == opponent:
-                             print("It's a Tie\n")
-                       elif move == 'Paper' and opponent == 'Scissors':
-                             print("You lose\n")
-                       elif move == 'Paper' and opponent == 'Rocks':
-                             print("You win\n")
-                       else:
-                             print("Invalid move Paper\n")
+               elif move == 'paper':
+                       paper(move,opponent)
 
-               elif move == 'Scissors':
-                       if move == opponent:
-                             print("It's a Tie\n")
-                       elif move == 'Scissors' and opponent == 'Paper':
-                             print("You win\n")
-                       elif move == 'Scissors' and opponent == 'Rocks':
-                             print("You lose\n")
-                       else:
-                             print("Invalid move Scissors")
+               elif move == 'scissors':
+                       scissor(move,opponent)
 
-               elif move == 'Exit':
+               elif move == 'exit' or move == "Exit":
                        print("Thank you for playing Speed Rocks,Paper,Scissor\n")
                        break
                else:
